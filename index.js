5ef7c4f83b8936987c135ed0360e16e9258398ab
18abhinavvk <abhinav.180410107126@gmail.com>
2022-10-04 16:07:53 +0530

    Commit Message

---
+++ index.js
@@ -0,0 +1,37 @@
+//Arithmetic Operators
+let x = 60;
+let y = 30;
+let z = x+y;
+let a = x-y;
+let b = x*y;
+let c = x/y;
+let d = x%y;
+console.log(z, a, b, c, d);
+
+//Logical Operators
+let e = x||y;
+let f = x&&y;
+let g = !y;
+console.log(e, f, g);
+
+//Bitwise Operators
+let h = x|y;
+let i = x&y;
+let j = x^y;
+let k = x<<2;
+let l = y>>2;
+let m =  x ?? y;
+console.log(h, i, j, k, l, m);
+
+//Comparision Operators
+let n = a>b;
+let o = a<b;
+let p = a==b;
+let q = a!=b;
+let r = a>=b;
+let s = a<=b;
+console.log(n, o, p, q, r, s);
+
+//Special Operators
+let t1 = a>b ? a:b;
+console.log(t1)
